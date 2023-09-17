5ac901f9f51240f6769ed381c4568eda6b0ea767
Barun Aaryan <barunaryan1@gmail.com>
2023-09-17 11:33:51 +0530

    Added a Lowercase button and a text summary button which tells us abouthe time taken to read the paragraph and other info like number of words and characters and a preview button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,6 @@
 import React , {useState} from 'react';
+import PropTypes from 'prop-types'
+s
 
 
 
@@ -8,17 +10,32 @@ export default function TextForm(props) {
         let newText= text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick=()=>{
+      //  console.log("Uppercase was clicked" + text);
+        let newText= text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value);
     }
     const [text, setText]= useState('Enter text here');
   return (
-<div>
+    <>
+<div className = "container">
     <h1>{props.heading}</h1>
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="8"></textarea>
 </div>
-<button className="button btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+<button className="button btn-primary mx-3" onClick={handleUpClick}>Convert to UpperCase</button>
+<button className="button btn-primary" onClick={handleLoClick}>Convert to LowerCase</button>
     </div>
+    <div className="container my-2">
+      <h1> Your text summary</h1>
+      <p> {text.split(" ").length} words and {text.length} characters</p>
+      <p> {0.008 * text.split(" ").length} Minutes read</p>
+      <h2> Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 } 
